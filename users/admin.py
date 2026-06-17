1bc8966575d505f5ca25b3bad0533bdfe6442c51
Jimi Wilson <jimiwilsondev@gmail.com>
2026-06-17 02:26:58 +0100

    Adding custom user model

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
+
 
 # Register your models here.
+
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    model = User
+
+    fieldsets = UserAdmin.fieldsets + (
+        ('KeepNet Profile Data', {'fields': ('bio', 'profile_picture')}),
+    )
